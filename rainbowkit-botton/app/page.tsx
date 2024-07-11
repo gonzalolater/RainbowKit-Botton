8a488f8390fcd5a2ed8e9485d8508dbb10862ef2
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2024-07-11 08:18:24 -0300

    Connect Wallet

--- rainbowkit-botton/app/page.tsx
+++ rainbowkit-botton/app/page.tsx
@@ -6,7 +6,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <ConnectButton label="Sign in"/>
+     <ConnectButton label="Connect Wallet"/>
     </main>
   );
 }
